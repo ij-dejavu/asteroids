c776e40fe211a058ae31099ef342bcea78fce8d0
ij-dejavu <inga.dejavu@gmail.com>
2025-03-21 19:08:50 +0000

    Correct game loop code and order in main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")   
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
-        pygame.display.update()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
